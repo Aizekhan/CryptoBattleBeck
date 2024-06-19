b3d06db43436e531694d74af399e577bfd58c08b
grucia3 <grucia3@gmail.com>
2024-06-19 23:07:09 +0300

    Updated URL to use environment variables

--- auth.js
+++ auth.js
@@ -1,12 +1,10 @@
-// backend/auth.js
-
-const passport = require('passport');
 const TelegramStrategy = require('passport-telegram-official').Strategy;
+const passport = require('passport');
 const User = require('./models/User');
 const config = require('./config');
 
 passport.use(new TelegramStrategy({
-    botToken: process.env.TELEGRAM_BOT_TOKEN
+    botToken: process.env.TELEGRAM_BOT_TOKEN,
 }, async (profile, done) => {
     try {
         let user = await User.findOne({ telegramId: profile.id });
@@ -14,19 +12,12 @@ passport.use(new TelegramStrategy({
             user = new User({
                 telegramId: profile.id,
                 username: profile.username,
-                displayName: profile.displayName
+                displayName: profile.displayName,
             });
             await user.save();
         }
         return done(null, user);
     } catch (err) {
-        return done(err);
+        return done(err, false);
     }
 }));
-
-app.get('/auth/telegram/callback', 
-    passport.authenticate('telegram', { failureRedirect: '/' }),
-    (req, res) => {
-        res.redirect('/');
-    }
-);
