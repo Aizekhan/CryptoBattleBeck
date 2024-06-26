13d7a98ee81b310697bea33e38f0595844a66c70
Bohdan <e5n.drive@gmail.com>
2024-06-27 01:56:27 +0300

    Changed user method

--- routes/user.js
+++ routes/user.js
@@ -125,7 +125,7 @@ router.post('/login', async (req, res) => {
 });
 
 // Отримання профілю користувача
-router.get(`/{userId}`, protect, async (req, res) => {
+router.get(`/:userId`, protect, async (req, res) => {
     console.log("Getting user by id", userId);
     const user = await User.findById(userId);
 
