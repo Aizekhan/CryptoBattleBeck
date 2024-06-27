1fb970cc3751b3b290049ef6611129ecdd54b83c
Bohdan <e5n.drive@gmail.com>
2024-06-27 20:18:58 +0300

    Changed cors policy headers2

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
+
 // Add CORS
-app.options('*', (req, res) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.send();
+    next();
   });
-
-app.options('*', cors());
 
 // Middleware
 app.use(express.json());
