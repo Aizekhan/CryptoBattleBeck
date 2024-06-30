eae1731d46d03ad17aae36dfb0cbd0f71da38de7
Bohdan <e5n.drive@gmail.com>
2024-06-30 18:32:57 +0300

    Changed CORS policy

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ dotenv.config();
 const app = express();
 
 // Add CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json({});
-      }
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-});
+  });
+  
+app.options('*', (req, res) => {
+    res.sendStatus(204);
+  });
 
 // Middleware
 app.use(express.json());
